af6a0009425b82d20a6bfc44aae556ddc80d84f2
Bas-Techasit <techasit.bas@gmail.com>
2023-03-27 17:29:53 +0700

    refactor get left-operand for second pattern

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 
+
 class Captcha {
 
-    private leftOperand: number;
+    private readonly leftOperand: number
+    private readonly rightOperand: number
+    private readonly operator: number
+    private readonly pattern: number
 
     constructor(pattern: number, leftOperand: number, operator: number, rightOperand: number) {
-        this.leftOperand = leftOperand;
+        this.leftOperand = leftOperand
+        this.rightOperand = rightOperand
+        this.operator = operator
+        this.pattern = pattern
     }
 
     public getLeftOperand(): string {
-        if (this.leftOperand === 9) return "9"
-        return "1"
+       if (this.pattern === 2) return numericString[this.leftOperand]
+        return this.leftOperand.toString()
     }
+
+    public getRightOperand(): string {
+       return numericString[this.rightOperand]
+    }
+
+    public getOperator(): string {
+        enum Operator { "+" = 1, "-", "*" }
+        return Operator[this.operator]
+    }
+
 }
+
+enum numericString {
+    "One" = 1,
+    "Two",
+    "Three" ,
+    "Four" ,
+    "Five" ,
+    "Six" ,
+    "Seven" ,
+    Eight ,
+    "Nine",
+}
+
 
 module.exports = Captcha
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,15 +1,76 @@
+import exp from "constants";
+
 const CaptCha = require('../index')
 
 
 describe('Captcha', function () {
 
-    test("Get left-operand should be 1", () => {
-        const captcha = new CaptCha(1, 1, 1, 1)
-        expect(captcha.getLeftOperand()).toBe("1")
+    describe('First Pattern', function () {
+
+        test("Get left-operand should be 1", () => {
+            const captcha = new CaptCha(1, 1, 1, 1)
+            expect(captcha.getLeftOperand()).toBe("1")
+       });
+
+        test('Get left-operand should be 5', () => {
+            const captcha = new CaptCha(1, 5, 1, 1)
+            expect(captcha.getLeftOperand()).toBe("5")
+        })
+
+        test('Get left-operand should be 9', () => {
+            const captcha = new CaptCha(1, 9, 1, 1)
+            expect(captcha.getLeftOperand()).toBe("9")
+        });
+
+        test('Get right-operand should be One', () => {
+            const captcha = new CaptCha(1, 1, 1, 1)
+            expect(captcha.getRightOperand()).toBe("One")
+        })
+
+        test('Get right-operand should be Nine', () => {
+            const captcha = new CaptCha(1, 1, 1, 9)
+            expect(captcha.getRightOperand()).toBe("Nine")
+        })
+
+        test('Get right-operand should be Five', () => {
+            const captcha = new CaptCha(1, 1, 1, 5)
+            expect(captcha.getRightOperand()).toBe("Five")
+        })
+
+        test('Get operator should be +', () => {
+            const captcha = new CaptCha(1, 1, 1, 1)
+            expect(captcha.getOperator()).toBe("+")
+        })
+
+        test('Get operator should be -', () => {
+            const captcha = new CaptCha(1, 1, 2, 1)
+            expect(captcha.getOperator()).toBe("-")
+        })
+
+        test('Get operator should be *', () => {
+            const captcha = new CaptCha(1, 1, 3, 1)
+            expect(captcha.getOperator()).toBe("*")
+        })
     });
 
-    test('Get left-operand should be 9', () => {
-       const captcha = new CaptCha(1, 9, 1, 1)
-       expect(captcha.getLeftOperand()).toBe("9")
+    describe('Second Pattern', function () {
+
+        test('Get left-operand should be One', () => {
+            const captcha = new CaptCha( 2, 1, 1, 1)
+            expect(captcha.getLeftOperand()).toBe('One')
+        })
+
+        test('Get left-operand should be Nine', () => {
+            const captcha = new CaptCha(2, 9, 1, 1)
+            expect(captcha.getLeftOperand()).toBe('Nine')
+        })
+
+        test('Get left-operand should be Five', () => {
+            const captcha = new CaptCha(2, 5, 1, 1)
+            expect(captcha.getLeftOperand()).toBe('Five')
+        })
+
+        
     });
+
 });
